1edc24dec0e07a0fd3af705c41d1ad287dcdd687
junbangg <wnstjr4620@naver.com>
2022-01-25 23:50:48 +0900

    Programmers: 신고 결과 받기 코드 추가

---
+++ Aladdin/Programmers/신고_결과_받기.swift
@@ -0,0 +1,39 @@
+import Foundation
+
+func split(from string: String) -> (String, String) {
+    let splitData = string.split(separator: " ").map { String($0) }
+    return (splitData[0], splitData[1])
+}
+
+func solution(_ id_list:[String], _ report:[String], _ k:Int) -> [Int] {
+    var answer = Array(repeating: 0, count: id_list.count)
+    var id_index: [String: Int] = [:]
+    var reported_reporters: [String: Set<String>] = [:]
+        
+    for (index, id) in id_list.enumerated() {
+        id_index[id] = index
+    }
+    
+//    report.forEach { data in
+    for data in report {
+        let (reporter, reported) = split(from: data)
+        
+        if reported_reporters[reported] != nil {
+            reported_reporters[reported]!.insert(reporter)
+        } else {
+            reported_reporters[reported] = [reporter]
+        }
+    }
+    
+//    reported_reporters.forEach { (_, reporters) in
+    for (_, reporters) in reported_reporters {
+        if reporters.count >= k {
+//            reporters.forEach { id in
+            for id in reporters {
+                answer[id_index[id]!] += 1
+            }
+        }
+    }
+    
+    return answer
+}
