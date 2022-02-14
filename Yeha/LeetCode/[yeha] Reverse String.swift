7ce5a602cab1e4e0384f26f6e9ec32c4ec70ebc2
Yeonhee Ha <dev.yeha@gmail.com>
2022-02-14 20:37:32 +0900

    leetcode: Reverse String

---
+++ Yeha/LeetCode/[yeha] Reverse String.swift
@@ -0,0 +1,16 @@
+class Solution {
+    func reverseString(_ s: inout [Character]) {
+        var left = 0
+        var right = s.count - 1
+        var temp: Character
+        
+        while left < right {
+            temp = s[left]
+            s[left] = s[right]
+            s[right] = temp
+            left += 1
+            right -= 1
+        }
+        print(s)
+    }
+}
