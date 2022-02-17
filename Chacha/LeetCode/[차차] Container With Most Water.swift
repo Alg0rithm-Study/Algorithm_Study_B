5979401bdd42ab4812ef9375c94bc1526637cfe3
chacha <45652743+ChaminLee@users.noreply.github.com>
2022-02-18 01:41:55 +0900

    leetcode: Container With Most Water

---
+++ Chacha/LeetCode/[차차] Container With Most Water.swift
@@ -0,0 +1,32 @@
+// Logic 
+// 1. x칸 차이 * 두 bar 중 더 낮은 높이 = 너비
+// 2. 양 끝에서 시작해서 너비를 구하면서 좁혀나가기
+// 3. 두 높이 중 더 낮은 높이에 해당하는 (좌, 우)를 이동시키기 
+// 4. 최대 너비를 반환
+
+// Solution
+class Solution {
+    func maxArea(_ height: [Int]) -> Int {
+        var left = 0
+        var right = height.count - 1
+        var maxWater = 0
+        
+        while left < right {
+            let leftHeight = height[left]
+            let rightHeight = height[right]
+            let curWater = min(leftHeight, rightHeight) * (right - left)
+            
+            if water > maxWater {
+                maxWater = w * h
+            } else {
+                if leftHeight > rightHeight {
+                    right -= 1
+                } else {
+                    left += 1
+                }
+            }
+        }
+        
+        return maxWater
+    }
+}
